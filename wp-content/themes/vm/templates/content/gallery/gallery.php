859be317e0ace67829001c18d48366e770260657
SamanthaClaes <Samantha.claes@student.hepl.be>
2025-05-20 15:55:02 +0200

    added variables scss

--- wp-content/themes/vm/templates/content/gallery/gallery.php
+++ wp-content/themes/vm/templates/content/gallery/gallery.php
@@ -6,19 +6,21 @@
 
 
 <section class="section_container">
-<div class="div_title_img">
-    <?= $headline ?>
-</div>
-
-<?php if ($gallery): ?>
-    <div class="gallery_flex_wrapper  ">
-        <?php foreach ($gallery as $image): ?>
-            <div class="div_container">
-                <?= responsive_image($image, ['classes' => 'img_gallery', 'lazy' => true]) ?>
-            </div>
-        <?php endforeach; ?>
+    <div class="div_title_img">
+        <?= $headline ?>
     </div>
-<?php else: ?>
-    <p>Image non disponible</p>
-<?php endif; ?>
+
+    <?php if ($gallery): ?>
+        <div class="gallery_flex_wrapper js-carousel">
+            <?php foreach ($gallery as $i => $image): ?>
+                <div class="div_container <?= $i === 0 ? ' active' : '' ?>">
+                    <?= responsive_image($image, ['classes' => 'img_gallery', 'lazy' => true]) ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
+
+    <?php else: ?>
+        <p>Image non disponible</p>
+    <?php endif; ?>
 </section>
+
